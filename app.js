354e0e29b2fed07f32b4ae614c8250d21f72630c
Saïsana <khamvongsa.v@hotmail.fr>
2025-08-14 12:24:08 +0200

    Route added to get books resources, and CORS error managed.

--- app.js
+++ app.js
@@ -3,22 +3,46 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log("reçus");
-    next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next ) => {
-    res.json('Hello World');
-    next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+app.use('/api/books', (req, res, next) => {
+  const books = [
+    {
+      userId: 'oeihfzeoi',
+      title: 'Mon premier livre',
+      author : 'MasterBook',
+      description: 'Les infos de mon premier livre',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      year: 1999,
+      genre: 'mystère',
+      ratings: [
+        {
+          userId: 'oeihfzeoi',
+          grade: 3,
+      }],
+      averageRating: 3,
+    },
+    {
+      userId: 'oeihfzeoifsdgfsd',
+      title: 'Mon deuxième livre',
+      author : 'LegendMan',
+      description: 'Les infos de mon premier livre',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      year: 2015,
+      genre: 'Aventure',
+      ratings: [
+        {
+          userId: 'oeihfzeoifsdgfsd',
+          grade: 4,
+      }],
+      averageRating: 4,
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
