88f22e5eca171c2a0b76a95ce85c2ed9d2e85546
Saïsana <khamvongsa.v@hotmail.fr>
2025-08-16 11:43:31 +0200

    Middleware Authentification added.

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const RANDOM_TOKEN_SECRET = process.env.RANDOM_TOKEN_SECRET;
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, RANDOM_TOKEN_SECRET);
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            req.auth = {
+                userId: userId
+            };
+            next();
+        }
+    } catch(error) {
+        res.status(401).json({error});
+    }
+};
--- routes/book.js
+++ routes/book.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+
+const auth = require('../middleware/auth');
+
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/bestrating', bookCtrl.getTop3RatingsBooks);
-router.post('/', bookCtrl.createBook);
-router.post('/:id/rating', bookCtrl.rateBook);
-router.put('/:id', bookCtrl.updateBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.post('/', auth, bookCtrl.createBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
+router.put('/:id', auth, bookCtrl.updateBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
